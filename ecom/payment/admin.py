4353df71ea9534494d5feaa93e7f4c007bcb07e4
Giacomo Orsucci <orsucci.giacomo@gmail.com>
2024-06-16 17:55:13 +0200

    created order model

--- ecom/payment/admin.py
+++ ecom/payment/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import ShippingAddress
+from .models import ShippingAddress, Order, OrderItem
 
 #to register the models on the admin panel
 
 admin.site.register(ShippingAddress)
+admin.site.register(Order)
+admin.site.register(OrderItem)
