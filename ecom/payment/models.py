7fc205c42bc27e004684b3eb54a5fe2e527c93b1
Giacomo Orsucci <orsucci.giacomo@gmail.com>
2024-06-17 13:14:12 +0200

    Added order checkout and fixed billing

--- ecom/payment/models.py
+++ ecom/payment/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
+from django.db.models.signals import post_save
 
 
 class ShippingAddress(models.Model):
@@ -21,6 +22,14 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return f'ShippingAddress - {str(self.id)}'
+
+#default shipping address when user signs up
+def create_shipping(sender, instance, created, **kwargs):
+    if created:
+        user_shipping = ShippingAddress(user=instance)
+        user_shipping.save()
+
+post_save.connect(create_shipping, sender=User)
 
 class Order(models.Model):
 
